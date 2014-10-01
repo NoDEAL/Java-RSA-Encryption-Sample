57452c74fef720bc68a6cfccd12f7af231606887
NoDEAL <kjiwhan0120@gmail.com>
2014-10-02 01:55:42 +0900

    Code formatted

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,24 @@
 import java.security.*;
 
 public class Main {
-	
-	public static void main(String[] args) throws Exception {
-		RSA rsa = new RSA();
-		RsaGenerateKey rsaGenerateKey = new RsaGenerateKey();
-		
-		Key rsaPublicKey = rsaGenerateKey.generatePublicKey();
-		Key rsaPrivateKey = rsaGenerateKey.generatePrivateKey();
-		
-		String plain = "Hello World!";
-		System.out.println("Plain text: " + plain + "\n" + plain.length());
-	
-		byte[] encryptedPlainByte = rsa.encrypt(rsaPublicKey, plain);
-		String encryptedPlainText = new String(encryptedPlainByte);
+
+    public static void main(String[] args) throws Exception {
+        RSA rsa = new RSA();
+        RsaGenerateKey rsaGenerateKey = new RsaGenerateKey();
+        
+        Key rsaPublicKey = rsaGenerateKey.generatePublicKey();
+        Key rsaPrivateKey = rsaGenerateKey.generatePrivateKey();
+        
+        String plain = "Hello World!";
+        System.out.println("Plain text: " + plain + "\n" + plain.length());
+    
+        byte[] encryptedPlainByte = rsa.encrypt(rsaPublicKey, plain);
+        String encryptedPlainText = new String(encryptedPlainByte);
 
         System.out.println("Encrypted Plain Text: " + encryptedPlainText + "\n" + encryptedPlainText.length());
 
-		byte[] decryptedByte = rsa.decrypt(rsaPrivateKey, encryptedPlainByte);
-		String decryptedText = new String(decryptedByte);
+        byte[] decryptedByte = rsa.decrypt(rsaPrivateKey, encryptedPlainByte);
+        String decryptedText = new String(decryptedByte);
         System.out.println("Decrypted text: " + decryptedText);
-	}
+    }
 }
--- src/RSA.java
+++ src/RSA.java
@@ -3,16 +3,16 @@ import java.security.spec.*;
 import javax.crypto.*;
 
 public class RSA {
-	
-	public byte[] encrypt(Key publicKey, String plainText) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-		return cipher.doFinal(plainText.getBytes());
-	}
-	
-	public byte[] decrypt(Key privateKey, byte[] encryptedByte) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException {
-		Cipher cipher = Cipher.getInstance("RSA");
-		cipher.init(Cipher.DECRYPT_MODE, privateKey);
-		return cipher.doFinal(encryptedByte);
-	}
+    
+    public byte[] encrypt(Key publicKey, String plainText) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        return cipher.doFinal(plainText.getBytes());
+    }
+    
+    public byte[] decrypt(Key privateKey, byte[] encryptedByte) throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        return cipher.doFinal(encryptedByte);
+    }
 }
