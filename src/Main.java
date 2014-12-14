033c433c08b26d136ed5c6c371c0eeeecde1a11a
nodeal <kjiwhan0120@gmail.com>
2014-12-15 02:44:01 +0900

    Changed to make output file, not printing in stdout

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,35 @@
+import java.io.*;
 import java.security.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        if (args.length == 0) {
+            System.out.println("No input\nUsage: java Main [plain text]");
+            System.exit(0);
+        }
+
         RSA rsa = new RSA();
         RsaGenerateKey rsaGenerateKey = new RsaGenerateKey();
         
         Key rsaPublicKey = rsaGenerateKey.generatePublicKey();
         Key rsaPrivateKey = rsaGenerateKey.generatePrivateKey();
         
-        String plain = "Hello World!";
-        System.out.println("Plain text: " + plain + "\n" + plain.length());
+        String plain = args[0];
     
         byte[] encryptedPlainByte = rsa.encrypt(rsaPublicKey, plain);
         String encryptedPlainText = new String(encryptedPlainByte);
-
-        System.out.println("Encrypted Plain Text: " + encryptedPlainText + "\n" + encryptedPlainText.length());
-
         byte[] decryptedByte = rsa.decrypt(rsaPrivateKey, encryptedPlainByte);
         String decryptedText = new String(decryptedByte);
-        System.out.println("Decrypted text: " + decryptedText);
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("output.txt"));
+        bufferedWriter.write("Plain text: " + plain);
+        bufferedWriter.newLine();
+        bufferedWriter.write("Encryped text: " + encryptedPlainText);
+        bufferedWriter.newLine();
+        bufferedWriter.write("Length: " + encryptedPlainText.length());
+        bufferedWriter.newLine();
+        bufferedWriter.write("Decrypted text: " + decryptedText);
+        bufferedWriter.close();
     }
 }
